4962d4d2c7958e03f8c5bc1512d84a464ec0e5fc
FCL00 <flagahit00@gmail.com>
2024-01-05 21:15:29 +0800

    DAY-5

---
+++ DAY-5/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center bg-[#181818]">
+        <div className="grid grid-cols-3 p-5 bg-gray-900">
+          <div className="first-col"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
